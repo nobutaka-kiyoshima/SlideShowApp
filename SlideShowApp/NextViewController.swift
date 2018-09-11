0e755fb6d85a8049096571281ca2b4f58bbbe97e
Nobutaka Kiyoshima <nobukiyo@nobutakas-macbook.local>
2018-09-11 18:43:20 +0700

    Bring forward Slide app again

--- SlideShowApp/NextViewController.swift
+++ SlideShowApp/NextViewController.swift
@@ -14,15 +14,13 @@ class NextViewController: UIViewController {
     var selectedImage: UIImage?
     
     override func viewDidLoad() {
-        super.viewDidLoad()
+        super.viewDidLoad()        
         
-        
-
         largerSlidePhoto.image  = selectedImage
         
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -30,13 +28,13 @@ class NextViewController: UIViewController {
     
     
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
--- SlideShowApp/ViewController.swift
+++ SlideShowApp/ViewController.swift
@@ -15,43 +15,55 @@ class ViewController: UIViewController {
     var index = 0
     
     
-    
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    
-    
-    let tapGesture:UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.tapped(_:)))
-    
-    SlidePhoto.image  = UIImage(named:imageNames [index])
         
+        // 定数tapGesture(UITapGestureRecognizerというクラス)にUITapGestureRecognizerのインスタンスを生成して代入。
+        let tapGesture:UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.tapped(_:)))
+        
+        //SlidePhotoに格納されているUIImageViewクラスの中のimageプロパティを呼び出して、そのimageプロパティの中にUIImageクラスに引数を代入して生成したインスタンスを代入
+        SlidePhoto.image  = UIImage(named:imageNames [index])
+       
+        //UIIMageViewのadd...メッソドを呼び出して、tapGestureを引数として代入
         self.SlidePhoto.addGestureRecognizer (tapGesture)
         
         SlidePhoto.isUserInteractionEnabled = true
+        
     }
-    
+    //定義　
     @objc func tapped(_ sender:UITapGestureRecognizer) {
         performSegue(withIdentifier: "toSecond", sender: nil)
         
+        //タイマー稼働中であれば、timerを停止し、更にnilを代入してタイマーそのものを無くしてスライドショーをストップする。これで、遷移先の拡大したスライドから戻る時、遷移前のスライドに戻ることになる。
+        if timer != nil {
+            timer.invalidate()
+            timer = nil
+            //PlayStopPlayプロパティに格納されているUIButtonクラスの中の、setTitleと云うボタンのタイトルを変えるメッソドを呼び出している
+            PlayStopPlay.setTitle ("再生", for: .normal)
+            //forwardButtonの中に格納されているUIButtonクラスのisEnabledプロパティにtrueを代入
+            forwardButton.isEnabled = true
+            backwardButton.isEnabled = true
+        }
+        
     }
-        
-        
+    
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         //Segueから遷移先のnextViewControllerを取得する。
         let nextViewController :NextViewController = segue.destination as! NextViewController
         
         nextViewController.selectedImage = UIImage (named: imageNames[index])
-        }
-        
+    }
+    
     @IBAction func unwind(_segue:UIStoryboardSegue) {
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    //画像のプロパティをSlidePhotoとの名前でコードと接続
+    //画像のプロパティをSlidePhotoとの名前でコードと接続　繋がってる印　プロパティとパーツがつながっている印
     @IBOutlet weak var SlidePhoto: UIImageView!
     
     @objc func updateTimer (_ timer: Timer) {
@@ -64,10 +76,10 @@ class ViewController: UIViewController {
     @IBOutlet weak var forwardButton: UIButton!
     @IBAction func GoForward(_ sender: Any) {
         index += 1
-         if index > 4 { index = 0}
+        if index > 4 { index = 0}
         SlidePhoto.image  = UIImage(named:imageNames [index])
         
-}
+    }
     //戻るボタン
     @IBOutlet weak var backwardButton: UIButton!
     @IBAction func GoBack(_ sender: Any) {
@@ -80,7 +92,6 @@ class ViewController: UIViewController {
     
     // 再生と停止ボタン
     @IBAction func PlayStop(_ sender: Any) {
-       
         //再生ボタンをタップすると2秒おきにカウントするタイマー
         //タイマーが起動中でなければ、タイマーを起動し、停止を表示
         if timer == nil {
@@ -89,14 +100,15 @@ class ViewController: UIViewController {
             forwardButton.isEnabled = false
             backwardButton.isEnabled = false
             //タイマーが起動中なら、停止ボタンを押して停止し、ボタンのタイトルを再生に切り替える
-        }else {self.timer.invalidate()
+        }else {
+            self.timer.invalidate()
             timer = nil
             PlayStopPlay.setTitle ("再生", for: .normal)
             forwardButton.isEnabled = true
             backwardButton.isEnabled = true
-            }
+        }
     }
-    }
+}
 
 
 
